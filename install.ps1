6efcb33647cd3949c7b2cdde20b13ef48b6193cf
BonBen365 <126295084+bonben365@users.noreply.github.com>
2023-04-06 06:38:23 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,89 +1,69 @@
 $MainMenu = {
-   Write-Host " ******************************************************"
-   Write-Host " * Microsoft Office Installation Script               *" 
-   Write-Host " * Date:    26/02/2023                                *" 
-   Write-Host " * Author:  https://github.com/bonben365              *" 
-   Write-Host " * Website: https://bonben365.com/                    *" 
-   Write-Host " ******************************************************" 
-   Write-Host 
-   Write-Host " 1. Office 365 / Microsoft 365" 
-   Write-Host " 2. Uninstall All Previous Versions of Microsoft Office"
-   Write-Host " 3. Quit or Press Ctrl + C"
-   Write-Host 
-   Write-Host " Select an option and press Enter: "  -nonewline
-   }
-   cls
-   
-   Do { 
-   cls
-   Invoke-Command $MainMenu
-   $Select = Read-Host
-   Switch ($Select)
-      {
-         #1. Office 365 / Microsoft 365
-          1 {
-               $365SubMenu = {
-               Write-Host " *************************************************"
-               Write-Host " * Select a Microsoft Office 365 Product         *" 
-               Write-Host " *************************************************" 
-               Write-Host 
-               Write-Host " 1. Office 365 Home" 
-               Write-Host " 2. Office 365 Personal"
-               Write-Host " 3. Microsoft 365 Apps for Business" 
-               Write-Host " 4. Microsoft 365 Apps for Enterprise" 
-               Write-Host " 5. Go Back"
-               Write-Host 
-               Write-Host " Select an option and press Enter: "  -nonewline
-               } 
-               cls
-                        
-            Do { 
+Write-Host " ******************************************************"
+Write-Host " * Microsoft Office 365 for Windows 7 Installation    *" 
+Write-Host " * Author: bonguides.com                              *" 
+Write-Host " * Updated on: 10/16/2022                             *" 
+Write-Host " ******************************************************" 
+Write-Host 
+Write-Host " 1. Install Office 32-bit" 
+Write-Host " 2. Install Office 64-bit"
+Write-Host " 3. Quit or Press Ctrl + C"
+Write-Host 
+Write-Host " Select an option and press Enter: "  -nonewline
+}
+cls
+
+$install = {
+   New-Item -Path $env:temp\temp -ItemType Directory -Force | Out-Null
+   cd $env:temp\temp
+   Write-Host 'Downloading........'
+   (New-Object System.Net.WebClient).DownloadFile("https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/bonguides.com/Scripts/O365_Windows7_$($arch)bit/setup.exe","$env:temp\temp\setup.exe")
+   (New-Object System.Net.WebClient).DownloadFile("https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/bonguides.com/Scripts/O365_Windows7_$($arch)bit/configuration.xml","$env:temp\temp\configuration.xml")
+   Write-Host 'Installing........'
+   .\setup.exe /configure .\configuration.xml
+   Write-Host 'Done........'
+}
+
+Do { 
+cls
+Invoke-Command $MainMenu
+$Select = Read-Host
+if ($select -eq 1) {$arch= '32'}
+if ($select -eq 1) {$arch= '64'}
+Switch ($Select)
+   {
+      #1. Install Office 32-bit
+       1 {
+            $365SubMenu = {
+            Write-Host " *************************************************"
+            Write-Host " * Select a Microsoft Office 365 Product         *" 
+            Write-Host " *************************************************" 
+            Write-Host 
+            Write-Host " 1. Office 365 Home" 
+            Write-Host " 2. Office 365 Personal"
+            Write-Host " 3. Microsoft 365 Apps for Business" 
+            Write-Host " 4. Microsoft 365 Apps for Enterprise" 
+            Write-Host " 5. Go Back"
+            Write-Host 
+            Write-Host " Select an option and press Enter: "  -nonewline
+            } 
             cls
-            Invoke-Command $365SubMenu
-            $365SubSelect = Read-Host
-   
-            if ($365SubSelect -eq 1) {$productId = "O365HomePremRetail"}
-            if ($365SubSelect -eq 2) {$productId = "O365HomePremRetail"}
-            if ($365SubSelect -eq 3) {$productId = "O365BusinessRetail"}
-            if ($365SubSelect -eq 4) {$productId = "O365ProPlusRetail"}
-   
-            $o365 = {
-               $null = New-Item -Path $env:temp\c2r -ItemType Directory -Force
-               Set-Location $env:temp\c2r
-               $fileName = "configuration.xml" 
-               New-Item $fileName -ItemType File -Force | Out-Null
-               Add-Content $fileName '<Configuration>'
-               Add-content $fileName '<Add OfficeClientEdition="64">'
-               Add-content $fileName "<Product ID=`"$productId`">"
-               Add-content $fileName '<Language ID="en-us" />'
-               Add-Content $fileName -Value '</Product>'
-               Add-Content $fileName -Value '</Add>'
-               Add-Content $fileName -Value '</Configuration>'
-               Write-Host
-               Write-Host ============================================================
-               Write-Host "Installing $productId...."
-               Write-Host ============================================================
-               Write-Host
-               $Download = join-path $env:temp\c2r setup.exe
-               (New-Object System.Net.WebClient).DownloadFile('https://filedn.com/lOX1R8Sv7vhpEG9Q77kMbn0/Files/7/setup.exe',$Download)
-               .\setup.exe /configure .\configuration.xml
-               # Cleanup
-               Set-Location "$env:temp"
-               Remove-Item $env:temp\c2r -Recurse -Force
-           }
-       
-           Switch ($365SubSelect)
-           {
-                1 { Invoke-Command $o365 }
-                2 { Invoke-Command $o365 }
-                3 { Invoke-Command $o365 }
-                4 { Invoke-Command $o365 }
-        
+                     
+         Do { 
+         cls
+         Invoke-Command $365SubMenu
+         $365SubSelect = Read-Host
+         Switch ($365SubSelect)
+                  {
+                     1 {Invoke-Command $install}
+                     2 {Invoke-Command $install}
+                     3 {Invoke-Command $install}
+                     4 {Invoke-Command $install}
+                     }
             }
-               }
-            While ($365SubSelect -ne 5)
-            cls
-            }  
-      }
+         While ($365SubSelect -ne 5)
+         cls
+         }  
    }
-   While ($Select -ne 3)
+}
+While ($Select -ne 3 )
